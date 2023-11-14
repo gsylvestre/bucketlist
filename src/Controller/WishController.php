7893f0f22d66a37c411fc55f2df6c22fdd2f1846
Guillaume <gsylvestre@gmail.com>
2023-11-14 16:11:53 +0100

    affichage de la liste des wish et du détail

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,23 +11,30 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     #[Route('/', name: 'wish_list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        //todo: va chercher les idées en bdd
+        //va chercher les idées en bdd
+        $wishes = $wishRepository->findBy(["isPublished" => true], ["dateCreated" => "DESC"]);
 
         return $this->render('wish/list.html.twig', [
-
+            "wishes" => $wishes
         ]);
     }
 
     #[Route('/{id}', name: 'wish_detail')]
-    public function detail(int $id): Response
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
         dump($id);
-        //todo: va chercher l'idée en bdd
+        //va chercher l'idée en bdd
+        $wish = $wishRepository->findOneBy(["isPublished" => true, "id" => $id]);
+
+        //par exemple, si le wish est supprimé ou n'est pas encore publié...
+        if (!$wish){
+            throw $this->createNotFoundException("This wish is not available! Sorry dude.");
+        }
 
         return $this->render('wish/detail.html.twig', [
-
+            "wish" => $wish
         ]);
     }
 }
---
+++ src/Entity/Wish.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WishRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WishRepository::class)]
+class Wish
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 250)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $author = null;
+
+    #[ORM\Column(nullable: true, options: ['default' => false])]
+    private ?bool $isPublished = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $dateCreated = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $dateUpdated = null;
+
+
+    public function __construct()
+    {
+        $this->isPublished = false;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function isIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(?bool $isPublished): static
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeImmutable
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated(\DateTimeImmutable $dateCreated): static
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    public function getDateUpdated(): ?\DateTimeImmutable
+    {
+        return $this->dateUpdated;
+    }
+
+    public function setDateUpdated(?\DateTimeImmutable $dateUpdated): static
+    {
+        $this->dateUpdated = $dateUpdated;
+
+        return $this;
+    }
+}
