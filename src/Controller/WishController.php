e795857cb39c384aea0d5877da6d4df3b79cb64e
Guillaume <gsylvestre@gmail.com>
2023-11-15 11:56:46 +0100

    formulaire de création et d'édition de Wish + suppression 🐱‍🐉

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,7 +25,7 @@ class WishController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'wish_detail')]
+    #[Route('/{id}', name: 'wish_detail', requirements: ['id' => '\d+'])]
     public function detail(int $id, WishRepository $wishRepository): Response
     {
         dump($id);
@@ -36,5 +40,70 @@ class WishController extends AbstractController
         return $this->render('wish/detail.html.twig', [
             "wish" => $wish
         ]);
+    }
+
+    #[Route('/create', name: 'wish_create', methods: ["GET", "POST"])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $wish = new Wish();
+        $wishForm = $this->createForm(WishType::class, $wish);
+
+        $wishForm->handleRequest($request);
+
+        if ($wishForm->isSubmitted() && $wishForm->isValid()){
+            $em->persist($wish);
+            $em->flush();
+
+            $this->addFlash("success", "Your wish has been granted!");
+            return $this->redirectToRoute("wish_detail", ["id" => $wish->getId()]);
+        }
+
+        return $this->render('wish/create.html.twig', [
+            "wishForm" => $wishForm,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'wish_edit', methods: ["GET", "POST"])]
+    public function edit(Wish $wish, Request $request, EntityManagerInterface $em): Response
+    {
+        $wishForm = $this->createForm(WishType::class, $wish);
+
+        $wishForm->handleRequest($request);
+
+        if ($wishForm->isSubmitted() && $wishForm->isValid()){
+            $wish->setDateUpdated(new \DateTimeImmutable());
+
+            $em->persist($wish);
+            $em->flush();
+
+            $this->addFlash("success", "The wish has been updated!");
+            return $this->redirectToRoute("wish_detail", ["id" => $wish->getId()]);
+        }
+
+        return $this->render('wish/edit.html.twig', [
+            "wish" => $wish,
+            "wishForm" => $wishForm,
+        ]);
+    }
+
+    #[Route("/{id}/delete/{token}", name: "wish_delete", requirements: ["id" => "\d+"], methods: ["GET"])]
+    public function delete(Wish $wish, string $token, EntityManagerInterface $em): Response
+    {
+        //voir dans le twig de la page détail pour la création du token
+        //ici on vérifie la validité manuellement du token csrf
+        $tokenIsValid = $this->isCsrfTokenValid("delete-token-" . $wish->getId(), $token);
+
+        //s'il est valide...
+        if($tokenIsValid){
+            $em->remove($wish);
+            $em->flush();
+
+            $this->addFlash("success", "The wish has vanished.");
+            return $this->redirectToRoute("wish_list");
+        }
+
+        //sinon...
+        $this->addFlash("danger", "The wish has NOT been deleted, sorry");
+        return $this->redirectToRoute("wish_detail", ["id" => $wish->getId()]);
     }
 }
--- src/Entity/Wish.php
+++ src/Entity/Wish.php
@@ -5,7 +5,10 @@ namespace App\Entity;
 use App\Repository\WishRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
+#[UniqueEntity("title", message: "This idea already exists!")]
 #[ORM\Entity(repositoryClass: WishRepository::class)]
 class Wish
 {
@@ -14,16 +17,30 @@ class Wish
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 250)]
+    #[Assert\NotBlank(message: "Please provide an idea!")]
+    #[Assert\Length(
+        min: 5, minMessage: "too short!",
+        max: 180, maxMessage: "too long! max 180..."
+    )]
+    #[ORM\Column(length: 180)]
     private ?string $title = null;
 
+    #[Assert\Length(
+        min: 5, minMessage: "too short!",
+        max: 2000, maxMessage: "too long! max 2000..."
+    )]
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $description = null;
 
+    #[Assert\NotBlank(message: "Please provide an username!")]
+    #[Assert\Regex(
+        pattern: "/^[a-z0-9_-]+$/i",
+        message: "Your username must match this pattern please: /^[a-z0-9_-]+$/i"
+    )]
     #[ORM\Column(length: 50)]
     private ?string $author = null;
 
-    #[ORM\Column(nullable: true, options: ['default' => false])]
+    #[ORM\Column(nullable: true, options: ['default' => true])]
     private ?bool $isPublished = null;
 
     #[ORM\Column]
@@ -35,7 +52,8 @@ class Wish
 
     public function __construct()
     {
-        $this->isPublished = false;
+        $this->isPublished = true;
+        $this->dateCreated = new \DateTimeImmutable();
     }
 
 
